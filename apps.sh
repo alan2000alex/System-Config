587170d95a8bb2bc0552c2555822c4edae426251
Alan <alan2000alex@gmail.com>
2020-08-14 11:55:14 +0530

    regular usage apps

---
+++ apps.sh
@@ -0,0 +1,39 @@
+sudo apt update -y
+
+#Telegram
+sudo apt install -y telegram-desktop
+
+#Element
+sudo wget -O /usr/share/keyrings/riot-im-archive-keyring.gpg https://packages.riot.im/debian/riot-im-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/riot-im-archive-keyring.gpg] https://packages.riot.im/debian/ default main" | sudo tee /etc/apt/sources.list.d/riot-im.list
+sudo apt update -y
+sudo apt install -y element-desktop
+
+#rocketchat
+# sudo snap install rocketchat-server
+# sudo snap install rocketchat-desktop
+
+#VSCode
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install -y code
+
+#Steam
+sudo apt install -y steam
+
+#Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+cat /etc/apt/sources.list.d/google-chrome.list
+
+#Tor browser
+wget -q -O - https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -
+echo "deb https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list
+sudo apt -y update
+sudo apt install -y tor deb.torproject.org-keyring torbrowser-launcher
+torbrowser-launcher
+
+#spotify
+sudo snap install spotify
+#android studio
+sudo snap install android-studio --classic
