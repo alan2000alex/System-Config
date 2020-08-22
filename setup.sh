dc7e23c6ef8fa9250f18721ae8e9277f3affddeb
Alan <alan2000alex@gmail.com>
2020-08-22 20:21:53 +0530

    Added speedtest

--- setup.sh
+++ setup.sh
@@ -52,6 +52,15 @@ sudo apt-get install -y libdbus-1-dev bluez libsbc-dev sbc-tools ffmpeg libavcod
 #sudo apt install -y cmake
 #sudo apt install -y bluetooth-dev
 
+#speedtest
+#sudo apt install -y gnupg1 apt-transport-https dirmngr
+#export INSTALL_KEY=379CE192D401AB61
+#export DEB_DISTRO=$(lsb_release -sc)
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+#echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
+
+sudo apt update -y
+sudo apt install -y speedtest
 sudo apt-get update -y
 sudo apt update -y
 sudo apt-get dist-upgrade -y
